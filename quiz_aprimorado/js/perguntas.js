62dbc90178257cd36d5027baec7e8981052faec2
Enéas Senna <50535733+eneas200@users.noreply.github.com>
2020-04-28 20:59:04 -0300

    atualização versao-0.2

---
+++ quiz_aprimorado/js/perguntas.js
@@ -0,0 +1,416 @@
+
+
+var QuizQuestoes = [
+	{
+		questao: "O que significa CPU?",
+		respostas: {
+			a: 'Control Panel Unit - Unidade de Painel de Controle',
+			b: 'Central Power Unit - Unidade de Força Central',
+            c: 'Control Power Unit - Unidade de Controle de Energia',
+            d: 'Central Processing Unit - Unidade Central de Processamento'
+		},
+		respostaCorreta: 'd'
+	},
+	{
+		questao: "Quais são as principais fabricantes de processadores para Desktops e notebooks ",
+		respostas: {
+			a: 'Intel e Dell',
+			b: 'Dell e AMD',
+			c: 'Intel e AMD',
+			d: 'Intel e IBM'
+		},
+		respostaCorreta: 'c'
+	},
+	{
+		questao: "Quais são os sintomas mais comuns de superaquecimento do processador?",
+		respostas: {
+			a: 'Apitos agudos da placa-mãe',
+			b: 'Reinicios e congelamentos inesperados da máquina',
+			c: 'Cheiro de queimado que exala de dentro do gabinete',
+			d: 'Imagem corrompida, com cores trocadas e manchas'
+		},
+		respostaCorreta: 'b'
+	},
+	{
+		questao: "O que significa a sigla “www” na internet?",
+		respostas: {
+			a: 'World wide web',
+			b: 'Web world wide',
+			c: 'Web wide world',
+			d: 'World web wide'
+		},
+		respostaCorreta: 'a'
+	},
+	{
+		questao: "A velocidade dos processadores atuais é normalmente medida em:",
+		respostas: {
+			a: 'Hertz ou Hz',
+			b: 'Gigahertz ou GHz',
+			c: 'Quilo byte ou Kb',
+			d: 'Megabyte ou MB'
+		},
+		respostaCorreta: 'b'
+	},
+	{
+		questao: "Qual foi a duração do primeiro vídeo do Youtube?",
+		respostas: {
+			a: '18 segundos',
+			b: '3 minutos',
+			c: '1 minuto',
+			d: '10 segundos'
+		},
+		respostaCorreta: 'a'
+	},
+	{
+		questao: "O número binário 10101010 equivale a qual número decimal apresentado",
+		respostas: {
+			a: '140',
+			b: '150',
+			c: '160',
+			d: '170'
+		},
+		respostaCorreta: 'd'
+	},
+	{
+		questao: "É um tipo de computador que tem como função controlar as demais estações de trabalho quando ligados em uma rede de computadores:Qual foi  o ano de lançamento do primeiro Iphone?",
+		respostas: {
+			a: '2008',
+			b: '2006',
+			c: '2007',
+			d: '2009'
+		},
+		respostaCorreta: 'c'
+	},
+	{
+		questao: "Qual foi a primeira rede social da história da internet?",
+		respostas: {
+			a: 'Classmate',
+			b: 'Myspace',
+			c: 'Orkut',
+			d: 'Facebook'
+		},
+		respostaCorreta: 'a'
+	},
+	{
+		questao: "Qual a resolução de uma imagem Full HD?",
+		respostas: {
+			a: '1920 x 1080',
+			b: '1280 x 720',
+			c: '2560 x 1440',
+			d: '800 x 960'
+		},
+		respostaCorreta: 'a'
+	},
+	{
+		questao: "O primeiro domínio registrado na internet foi:",
+		respostas: {
+			a: 'www.datacenter.com',
+			b: 'www.symbolics.com',
+			c: 'www.social.com',
+			d: 'www.us.gov'
+		},
+		respostaCorreta: 'b'
+	},
+	{
+		questao: "Quais destas não é uma linguagem de programação:",
+		respostas: {
+			a: 'Cobol',
+			b: 'Basic',
+			c: 'Smalltalk',
+			d: 'Preview'
+		},
+		respostaCorreta: 'd'
+	},
+	{
+		questao: "A unidade de medida da resolução de uma impressora e dada em:",
+		respostas: {
+			a: 'CPS',
+			b: 'BPM',
+			c: 'PPM',
+			d: 'DPI'
+		},
+		respostaCorreta: 'd'
+	},
+	{
+		questao: "Em modo de espera, quais componentes ficam ligadas?",
+		respostas: {
+			a: 'Placa mãe, processador, monitor',
+			b: 'Placa mãe, hd, monitor',
+			c: 'Placa mãe,memoria ram',
+			d: 'Placa mãe, Placa de rede, modem, memoria ram'
+		},
+		respostaCorreta: 'd'
+	},
+	{
+		questao: "Qual o país de origem da Samsung e LG?",
+		respostas: {
+			a: 'China e Japão',
+			b: 'China e China',
+			c: 'Coreia do Sul e Coreia do Sul',
+			d: 'Coreia do Sul e China'
+		},
+		respostaCorreta: 'c'
+	},
+	{
+		questao: "Qual a resolução de uma imagem em 4k?",
+		respostas: {
+			a: '3840 x 2160 pixels',
+			b: '3845 x 2250 pixels',
+			c: '3940 x 2140 pixels',
+			d: '7680 x 4320 pixels'
+		},
+		respostaCorreta: 'a'
+	},
+	{
+		questao: "Qual protocolo realiza o envio de mensagens de E-mail?",
+		respostas: {
+			a: 'POP, IMAP e CPMF',
+			b: 'POP, IMAP e SMTP',
+			c: 'CHFI, AHDi e QQLA',
+			d: 'FTP, HTTP e TCP'
+		},
+		respostaCorreta: 'b'
+	},
+	{
+		questao: "Qual a marca de processadores mais utilizadas atualmente nos smartphones Android?",
+		respostas: {
+			a: 'Qualcomm e MediaTek',
+			b: 'Vishera Black e Pentium',
+			c: 'Gold e LGA',
+			d: 'Apple Ax e MediaTek'
+		},
+		respostaCorreta: 'a'
+	},
+	{
+		questao: "Entre os meios de transmissão abaixo, assinale aquele que não é guiado:",
+		respostas: {
+			a: 'Par trançado',
+			b: 'Cabo coaxial',
+			c: 'Microondas',
+			d: 'Fibra ótica'
+		},
+		respostaCorreta: 'c'
+	},
+	{
+		questao: "fibra ótica",
+		respostas: {
+			a: '1980',
+			b: '1975',
+			c: '1975',
+			d: '1988'
+		},
+		respostaCorreta: 'a'
+	},
+	{
+		questao: "Quando foi lançado o primeiro microprocessador?",
+		respostas: {
+			a: 'Em 1971, havendo 3 concorrentes',
+			b: 'Em 1985, pela ARM Foundation',
+			c: 'Em 1979, pela Motorola',
+			d: 'Em 1944, durante a Segunda Guerra Mundial'
+		},
+		respostaCorreta: 'a'
+	},
+	{
+		questao: "O nome original do primeiro Xbox era:",
+		respostas: {
+			a: 'DirectBox',
+			b: 'BillBox',
+			c: 'ZBox',
+			d: 'MicrosoftBox'
+		},
+		respostaCorreta: 'a'
+	},
+	{
+		questao: "Quais são as principais fabricantes de processadores ARM?",
+		respostas: {
+			a: 'Samsung, PowerVR, AMD Tech e Intel',
+			b: 'Infineon Imaging, Texas Qualcomm e NVIDIA',
+			c: 'PowerVR, SGX, NVIDIA e Qualcomm Instruments',
+			d: 'Samsung, Texas Instruments, Qualcomm e NVIDIA'
+		},
+		respostaCorreta: 'd'
+	},
+	{
+		questao: "O que é firmware?",
+		respostas: {
+			a: 'Interface básica do sistema operacional do Macintosh',
+			b: 'Conjunto de instruções para o funcionamento de um dispositivo',
+			c: 'Byte existente no inicio de uma trilha de disco',
+			d: 'É um tipo de memória não volátil'
+		},
+		respostaCorreta: 'b'
+	},
+	{
+		questao: "FSB (Front Size Bus), também conhecido como barramento frontal, realiza a ligacao entre:",
+		respostas: {
+			a: 'Processador (CPU) e o chipset',
+			b: 'Chipset e a memoria RAM',
+			c: 'BIOS e a memória RAM',
+			d: 'Chipset e a placa-mãe'
+		},
+		respostaCorreta: 'a'
+	},
+	{
+		questao: "Em relação as topologias de redes, é correto afirmar que:",
+		respostas: {
+			a: 'Em relação as topologias de redes, é correto afirmar que:',
+			b: 'rede em anel consiste em estações conectadas através de um caminho fechado.',
+			c: 'redes por chaveamento de pacotes cada estação so pode realizar uma unica tarefa.',
+			d: 'topologia em estrela a falha de qualquer das estações causa a parada total do sistema.'
+		},
+		respostaCorreta: 'c'
+	},
+	{
+		questao: "Em qual ano foi criada a internet?",
+		respostas: {
+			a: '1982',
+			b: '1969',
+			c: '1950',
+			d: '1988'
+		},
+		respostaCorreta: 'b'
+	},
+	{
+		questao: "9.	Na terminologia usual de microinformática, os dados Pentium IV, 3.0 Ghz, 256 Mb RAM, HD 40 Gb, teclado padrão ABNT, placa de vídeo 128 mb AGP, constituem um modelo de:",
+		respostas: {
+			a: 'Organização',
+			b: 'Layout',
+			c: 'Configuração',
+			d: 'Estruturação'
+		},
+		respostaCorreta: 'c'
+	},
+	{
+		questao: "São computadores usados em ambiente de rede para aumentar a produtividade dos usuários em uma empresa, normalmente disponibilizados a esses funcionários dentro de um departamento. Uma das características é o não uso de disco rígido. Refere-se a qual tecnologia?",
+		respostas: {
+			a: 'Notebook',
+			b: 'Thinclient',
+			c: 'Mainframe',
+			d: 'Servidor'
+		},
+		respostaCorreta: 'b'
+	},
+	{
+		questao: "Quem foi o fundador da W3C (World Wide Web Consortium) ?",
+		respostas: {
+			a: 'Tim Berners-Lee',
+			b: 'Ray Tomlinson',
+			c: 'Dennis Ritchie',
+			d: 'Donald Knuth'
+		},
+		respostaCorreta: 'a'
+	}
+];
+
+// var re;
+// var quizContainer = document.getElementById('quiz');
+// var resultado = document.getElementById('results');
+// var submitButton = document.getElementById('submit');
+
+// gera_quiz(QuizQuestoes, quizContainer, resultado, submitButton);
+
+
+
+
+
+// function gera_quiz(questoes, quizContainer, resultado, submitButton){
+
+// 	function gera_questoes(questoes, quizContainer){
+        
+//         //armazenas saida
+//         var output = [];
+//         //armazena opçoes de entrada
+// 		var respostas; 
+
+		
+// 		for(var i=0; i<questoes.length; i++){
+			
+// 			//reseta a lista de questões
+// 			respostas = [];
+
+// 			// para cada resposta na questão.
+// 			for(letra in questoes[i].respostas){
+
+//                 // botões no html
+                
+// 				respostas.push(
+// 					'<label>' + '<br>' + '<div id="alt">'
+// 						+ '<input type="radio" style="margin-left:2em" name="questao'+i+'" value="' +letra+ '">'+'&nbsp'
+                    
+// 						+ questoes[i].respostas[letra] + '<div>'
+// 					+ '</label>'
+// 				);
+			
+// 			}
+
+// 			// adiciona questao e suas respostas à saída
+// 			output.push(
+// 				'<div class="questao" style="margin-top:2em">' + questoes[i].questao + '</div>'+'<br>'
+// 				+ '<div class="respostas">' + respostas.join('') + '</div>'+'<br>'
+// 			);
+
+			
+// 		}
+
+
+// 		// lista de saída em uma string de html e que são colocadas na página
+// 		quizContainer.innerHTML = output.join('');
+// 		document.getElementById('submit').style.position = "fixed";
+// 		document.getElementById('submit').style.display = "block" ;
+// 		document.getElementById('submit').style.bottom = "0";
+// 		document.getElementById('submit').style.marginLeft = "auto"
+// 		document.getElementById('submit').style.marginRigth = "auto";
+// 	}
+
+
+// 	function showResults(questoes, quizContainer, resultado){
+			
+		
+// 		var answerContainers = quizContainer.querySelectorAll('.respostas');
+		
+		
+// 		var userAnswer = '';
+// 		var numCorrect = 0;
+		
+		
+// 		for(var i=0; i<questoes.length; i++){
+
+// 			// agora ele vai virar a letra que esta marcada.
+// 			userAnswer = (answerContainers[i].querySelector('input[name=questao'+i+']:checked')||{}).value;
+			
+			
+			
+// 			if(userAnswer===questoes[i].respostaCorreta){
+				
+// 				numCorrect++;
+				
+				
+// 				answerContainers[i].style.color = 'lightgreen';
+// 			}
+			
+// 			else{
+				
+// 				answerContainers[i].style.color = 'red';
+// 			}
+// 		}
+
+// 			re =  '<div id="exibe_resultado"> <p id="resultado">Resultado</p>' + numCorrect + ' de ' + questoes.length; + '</div>'
+			
+// 		}
+
+	
+// 	gera_questoes(questoes, quizContainer);
+
+
+// 	//contabiliza pontos e mostra resultado
+// 	submitButton.onclick = function(){
+// 		showResults(questoes, quizContainer, resultado);
+// 		document.getElementById('quiz').innerHTML = re;
+// 		document.getElementById('submit').innerHTML = "Sair"
+// 		submitButton.onclick = function(){
+// 			location.href=" login.php";
+// 		}
+// 	}
+// }
+
